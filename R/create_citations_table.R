af841be1233caf8733fb2475df7dd132ef0d2c52
Levi Waldron <lwaldron.research@gmail.com>
2019-11-15 18:16:48 +0100

    create citations table, closes #6

---
+++ R/create_citations_table.R
@@ -0,0 +1,22 @@
+#' create citation table
+#'
+#' This will be the `citation` data model
+#'
+#' @importFrom dplyr select
+#' @importFrom magrittr %>%
+#' @param curation_sheet a data.frame, the curation worksheet from google sheet (sheet 2 as of now)
+#'
+#' @export
+create_citation_table = function(sheet = curation_sheet()) {
+  output <- sheet %>%
+    dplyr::select(PMID) %>%
+    dplyr::slice(-1) %>% #first row is comments
+    dplyr::filter(!is.na(PMID)) %>%
+    unique()
+  # fill in NAs for missing optional columns?
+  output$DOI <- rep(NA, nrow(output))
+  output$BibTex <- rep(NA, nrow(output))
+  output$URI <- rep(NA, nrow(output))
+  output
+}
+   
